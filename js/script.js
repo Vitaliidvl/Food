57f993fd3e936576192c4d6df42514c63b180fa0
Vitalii <kicker198686@gmail.com>
2020-11-02 19:05:45 +0200

    using rest operator

--- js/script.js
+++ js/script.js
@@ -152,12 +152,13 @@ function closeModal () {
 //Використання классів для карточок
 
  class MenuCard {
-     constructor(src, alt, title, descr, price, parentSelector) {
+     constructor(src, alt, title, descr, price, parentSelector, ...classes) {
          this.src = src;
          this.alt = alt;
          this. title = title;
          this.descr = descr;
          this.price = price;
+         this.classes = classes;
          this.parent = document.querySelector(parentSelector);
          this.transfer = 29;
          this.changeToUAH();
@@ -168,17 +169,26 @@ function closeModal () {
      }
      render() {
         const element = document.createElement('div');
+        if (this.classes.length === 0){
+            this.element = 'menu__item';
+            element.classList.add(this.element);
+        } else {
+        this.classes.forEach(className => element.classList.add(className));
+
+        }
+
         element.innerHTML = `
-        <div class="menu__item">
-                <img src=${this.src} alt=${this.alt}>
-                <h3 class="menu__item-subtitle">${this.title}</h3>
-                <div class="menu__item-descr">${this.descr}</div>
-                <div class="menu__item-divider"></div>
-                <div class="menu__item-price">
-                <div class="menu__item-cost">Цена:</div>
+            <img src=${this.src} alt=${this.alt}>
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">${this.descr}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
             <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
         </div>
-    </div>
+    ,
+    'menu__item',
+    'big'
     `;
     this.parent.append(element);
     }
@@ -214,6 +224,7 @@ function closeModal () {
     'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
     21,
     '.menu .container'
+    
 
 
  ).render(); 
