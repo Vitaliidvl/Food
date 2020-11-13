795bcdbe85edcaf76d87388d407a217261336afc
Vitalii <kicker198686@gmail.com>
2020-11-13 17:28:14 +0200

    axios

--- js/script.js
+++ js/script.js
@@ -205,6 +205,13 @@ window.addEventListener('DOMContentLoaded', () => {
         return await res.json();
     };
 
+    axios.get('http://localhost:3000/menu')
+    .then(data => {
+        data.data.forEach(({img, altimg, title, descr, price}) => {
+         new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+        }); 
+    });
+
     // getResource('http://localhost:3000/menu')
     // .then(data => {
     //    data.forEach(({img, altimg, title, descr, price}) => {
@@ -212,27 +219,27 @@ window.addEventListener('DOMContentLoaded', () => {
     //    }); 
     // });
 
-    getResource('http://localhost:3000/menu')
-    .then(data => createCard(data));
+    // getResource('http://localhost:3000/menu')
+    // .then(data => createCard(data));
 
-    function createCard(data) {
-        data.forEach(({img, altimg, title, descr, price}) => {
-    const element = document.createElement('div');
-    const price2 = (price * 28.6).toFixed();
-    element.classList.add('menu__item');
-    element.innerHTML = `
-    <img src=${img} alt=${altimg}>
-        <h3 class="menu__item-subtitle">${title}</h3>
-        <div class="menu__item-descr">${descr}</div>
-        <div class="menu__item-divider"></div>
-        <div class="menu__item-price">
-        <div class="menu__item-cost">Цена:</div>
-        <div class="menu__item-total"><span>${price2}</span> грн/день</div>
-    </div>
-    `;
-        document.querySelector('.menu .container').append(element);
-        });
-    }
+    // function createCard(data) {
+    //     data.forEach(({img, altimg, title, descr, price}) => {
+    // const element = document.createElement('div');
+    // const price2 = (price * 28.6).toFixed();
+    // element.classList.add('menu__item');
+    // element.innerHTML = `
+    // <img src=${img} alt=${altimg}>
+    //     <h3 class="menu__item-subtitle">${title}</h3>
+    //     <div class="menu__item-descr">${descr}</div>
+    //     <div class="menu__item-divider"></div>
+    //     <div class="menu__item-price">
+    //     <div class="menu__item-cost">Цена:</div>
+    //     <div class="menu__item-total"><span>${price2}</span> грн/день</div>
+    // </div>
+    // `;
+    //     document.querySelector('.menu .container').append(element);
+    //     });
+    // }
 
     
     //Forms
